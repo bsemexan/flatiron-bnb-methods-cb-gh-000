0e988361b76e02b9830493354f1034a147a004e2
Berry  Semexan <bsemexan@gmail.com>
2018-04-01 02:03:50 +0000

    automatically backed up by learn

--- app/models/concerns/reserve.rb
+++ app/models/concerns/reserve.rb
@@ -22,7 +22,7 @@ module Reserve
   class_methods do
     def highest_ratio_res_to_listings
       self.all.max_by do |ratio|
-        a.ratio_reservations_to_listings <=> ratio.ratio_reservations_to_listings
+        ratio.ratio_reservations_to_listings <=> ratio.ratio_reservations_to_listings
       end
     end
 
